08e27451907fd8ca185484f151b63551c92e7430
adi-kgp <adi.kgp@gmail.com>
2021-02-08 13:08:47 +0530

    added confetti on win functionality and converting a script to module code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {startConfetti, stopConfetti, removeConfetti} from './confetti.js';
 
 const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
@@ -35,7 +36,9 @@ let playerScoreNumber = 0;
 function resetSelected(){
   allGameIcons.forEach((icon) => {
     icon.classList.remove('selected');
-  })
+  });
+  stopConfetti();
+  removeConfetti();
 } 
 
 // Reset Score and playerChoice/computerChoice
@@ -49,6 +52,8 @@ function resetAll(){
   resultText.textContent = '';
   resetSelected();
 }
+
+window.resetAll =  resetAll;
 
 // Random computer choice
 function computerRandomChoice(){
@@ -101,6 +106,7 @@ function updateScore(playerChoice){
   } else {
     const choice = choices[playerChoice];
     if (choice.defeats.indexOf(computerChoice) > -1) {
+      startConfetti();
       resultText.textContent =  "You Won!";
       playerScoreNumber++;
       playerScoreEl.textContent = playerScoreNumber;
@@ -149,6 +155,8 @@ function select(playerChoice){
       break;
   }
 }
+window.select = select;
 
 // on startup, set initial values
 resetAll();
+
